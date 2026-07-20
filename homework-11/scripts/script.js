a973d9e95e448257c197b195e11ef1c70ca538a7
teonamgebrishvili <mgebrishvili.teona22@gmail.com>
2026-07-20 13:12:55 -0700

    Update script.js

--- homework-11/scripts/script.js
+++ homework-11/scripts/script.js
@@ -2,4 +2,10 @@ let birthYear = 1995;
 let currentYear = 2026;
 let age = currentYear - birthYear;
 
-console.log(age);
+if (typeof age !== "number" || isNaN(age)) {
+    console.log("მითითებული ასაკი არასწორია!");
+} else if (age < 18) {
+    console.log("თქვენ არ შეგიძლიათ მართვის მოწმობის აღება");
+} else {
+    console.log("თქვენ შეგიძლიათ მართვის მოწმობის აღება");
+}
